5feed99ea0763ed386d84555ea81c70b7051a0c8
md-mostafa <md.mostafa.akash@gmail.com>
2021-07-27 14:32:29 +0600

    find substring within a string

---
+++ 21.py
@@ -0,0 +1,9 @@
+#find substring within a string
+
+import re
+
+text = 'Python exercises, PHP exercises, C# exercises'
+pattern = 'exercises'
+
+for match in re.findall(pattern, text):
+    print('Found "%s"' %match)
